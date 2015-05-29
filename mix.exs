0e046b108c5c758dd007a235d3adce89eeeab8d8
Bruce Tate <bruce@rapidred.com>
2015-05-29 07:29:20 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pipes.Mixfile do
 
   def project do
     [ app: :pipe,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.0",
       deps: deps,
       package: package,
